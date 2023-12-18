19a04379a8e03840f52a1690448d92a26d8068c3
Alvaro Denis <denisacostaq@gmail.com>
2023-12-18 19:24:31 +0200

    Use function instead of lambda

--- src/main/java/com/github/denisacostaq/demo/MainVerticle.java
+++ src/main/java/com/github/denisacostaq/demo/MainVerticle.java
@@ -2,18 +2,25 @@ package com.github.denisacostaq.demo;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
         Router router = Router.router(vertx);
-        router.get("/api/v1/hello").handler(ctx -> ctx.response().end("Hello World!"));
-        router.get("/api/v1/hello/:name").handler(ctx -> {
-            String name = ctx.pathParam("name");
-            ctx.response().end(String.format("Hello %s!", name));
-        });
+        router.get("/api/v1/hello").handler(this::helloVertx);
+        router.get("/api/v1/hello/:name").handler(this::helloName);
         vertx.createHttpServer().requestHandler(router).listen(8080);
+    }
+
+    void helloVertx(RoutingContext ctx) {
+        ctx.request().response().end("Hello World!");
+    }
+
+    void helloName(RoutingContext ctx) {
+        String name = ctx.request().getParam("name");
+        ctx.request().response().end(String.format("Hello %s!", name));
     }
 
 }
