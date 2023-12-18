1c5df48d455f60a85056a01437de2bf551433335
Alvaro Denis <denisacostaq@gmail.com>
2023-12-18 18:55:49 +0200

    `mvn io.reactiverse:vertx-maven-plugin::setup`

---
+++ src/main/java/com/github/denisacostaq/demo/MainVerticle.java
@@ -0,0 +1,12 @@
+package com.github.denisacostaq.demo;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
