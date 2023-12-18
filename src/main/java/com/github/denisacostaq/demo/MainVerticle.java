11ab82338edf83fbe10fce26b6150684dcb3bb34
Alvaro Denis <denisacostaq@gmail.com>
2023-12-18 19:13:53 +0200

    handle path params

--- src/main/java/com/github/denisacostaq/demo/MainVerticle.java
+++ src/main/java/com/github/denisacostaq/demo/MainVerticle.java
@@ -9,6 +9,10 @@ public class MainVerticle extends AbstractVerticle {
     public void start() {
         Router router = Router.router(vertx);
         router.get("/api/v1/hello").handler(ctx -> ctx.response().end("Hello World!"));
+        router.get("/api/v1/hello/:name").handler(ctx -> {
+            String name = ctx.pathParam("name");
+            ctx.response().end(String.format("Hello %s!", name));
+        });
         vertx.createHttpServer().requestHandler(router).listen(8080);
     }
 
