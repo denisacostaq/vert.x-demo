ee88d533b15c914d684c21381d698d51a44f4bbe
Alvaro Denis <denisacostaq@gmail.com>
2023-12-18 19:06:44 +0200

    Introduce vertx-web, use router

--- src/main/java/com/github/denisacostaq/demo/MainVerticle.java
+++ src/main/java/com/github/denisacostaq/demo/MainVerticle.java
@@ -1,12 +1,15 @@
 package com.github.denisacostaq.demo;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-
+        Router router = Router.router(vertx);
+        router.get("/api/v1/hello").handler(ctx -> ctx.response().end("Hello World!"));
+        vertx.createHttpServer().requestHandler(router).listen(8080);
     }
 
 }
